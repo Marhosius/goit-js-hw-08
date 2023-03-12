0053123bb6e8af6db0ffc15c34860174ee9218bd
Marhosius <marhosius@gmail.com>
2023-03-12 22:43:08 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+// const iframe = document.querySelector('#vimeo-player');
+// const iframePlayer = new Vimeo.Player(iframe);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+let savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(savedTime);
+
+function onTimeUpdate() {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+        console.log(seconds)
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+const trotledonTimeUpdate = throttle(onTimeUpdate, 1000);
+
+player.on('timeupdate', trotledonTimeUpdate);
+
+
+
 
